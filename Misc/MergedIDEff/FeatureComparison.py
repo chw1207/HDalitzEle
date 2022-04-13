8b4f7981f475c17283611be15c475b8bae9c2f00
Cheng-Han <cheng-han.wu@cern.ch>
2022-04-13 19:31:32 +0800

    update the comparison under different pT bin

--- Misc/MergedIDEff/FeatureComparison.py
+++ Misc/MergedIDEff/FeatureComparison.py
@@ -4,6 +4,8 @@ import json
 import numpy as np
 from plugins.CMS_lumi import CMS_lumi
 
+# script to check the signal and zg have similar distributions among the training features
+
 
 def Draw1DHist(c, vh, vc=[None, None], vcl=["#E69D45", "#0061a8"], xaxis="x-axis", yaxis="x-axis", option="hist", y_axisscale=1.3, Log=False):
     ROOT.gPad.SetRightMargin(0.05)
@@ -51,7 +53,26 @@ def Draw1DHist(c, vh, vc=[None, None], vcl=["#E69D45", "#0061a8"], xaxis="x-axis
             h.Draw("%s same" %(option))
 
 
-def main():
+def main(pT_do, pT_up, region):
+    # selections of zg
+    sel_Zg = "&&".join([
+        "convVtxRadius_lep1 < {}".format(rconv),
+        "phoCalibEt_lep1 < {} && phoCalibEt_lep1 > {}".format(pT_up, pT_do),
+        "isHggPho_lep1 == 1",
+        "isEBPho_lep1 == 1"
+    ])
+    rdf_Zg_tmp = rdf_Zg.Filter(sel_Zg).Define("weight", "mcwei * puwei")
+
+    # selections of signal
+    sel_Si = "&&".join([
+        "eleCalibPt_lep1 < {} && eleCalibPt_lep1 > {}".format(pT_up, pT_do),
+        "elePresel_lep1 == 1",
+        "abs(eleSCEta_lep1) < 1.4442",
+        "category == 2"
+    ])
+    rdf_Si_tmp = rdf_Si.Filter(sel_Si).Define("weight", "mcwei * puwei")
+
+    # Draw the distributions
     with open("FeaturePlotParam_MergedIDEB2Gsf.json", "r") as fp:
         myTags = json.load(fp)
 
@@ -59,8 +80,8 @@ def main():
             setlogy = True if par["logy"] == "true" else False
 
             bininfo = par["bininfo"]
-            h1 = rdf_Zg.Histo1D(("h1_{}".format(tagName), " ", bininfo[0], bininfo[1], bininfo[2]), "{}_lep1".format(tagName), "weight").GetPtr()
-            h2 = rdf_Si.Histo1D(("h2_{}".format(tagName), " ", bininfo[0], bininfo[1], bininfo[2]), "{}_lep1".format(tagName), "weight").GetPtr()
+            h1 = rdf_Zg_tmp.Histo1D(("h1_{}".format(tagName), " ", bininfo[0], bininfo[1], bininfo[2]), "{}_lep1".format(tagName), "weight").GetPtr()
+            h2 = rdf_Si_tmp.Histo1D(("h2_{}".format(tagName), " ", bininfo[0], bininfo[1], bininfo[2]), "{}_lep1".format(tagName), "weight").GetPtr()
             h1.Scale(1./h1.Integral())
             h2.Scale(1./h2.Integral())
 
@@ -81,13 +102,19 @@ def main():
             leg.SetLineColor(0)
             leg.Draw()
 
+            ltx1 = ROOT.TLatex()
+            ltx1.SetNDC()
+            ltx1.SetTextFont(42)
+            ltx1.SetTextSize(0.04)
+            ltx1.DrawLatex(0.65, 0.8, "P_{T}: %d to %d GeV" %(pT_do, pT_up))
+
             c1.RedrawAxis()
 
-            outRes = "./plots"
+            outRes = "./plots/{}".format(region)
             if not os.path.exists(outRes):
                 os.makedirs(outRes)
-            c1.Print("{}/FeatureComparison_{}.pdf".format(outRes, tagName))
-            c1.Print("{}/FeatureComparison_{}.png".format(outRes, tagName))
+            c1.Print("{}/FeatureComparison_{}_{}.pdf".format(outRes, tagName, region))
+            c1.Print("{}/FeatureComparison_{}_{}.png".format(outRes, tagName, region))
             c1.Close()
 
 
@@ -95,22 +122,9 @@ if __name__ == "__main__":
     rdf_Zg = ROOT.RDataFrame("outTree", "./miniTree/2017/miniTree_ZGToLLG_2017.root")
     rdf_Si = ROOT.RDataFrame("outTree", "../GENstudy/minitree/2017/Minitree_HDalitz_*_m125_*.root")
 
-    rconv = 20
-    sel_den = "&&".join([
-        "convVtxRadius_lep1 < {}".format(rconv),
-        # "eleSCRawEn_lep1 < 65 && eleSCRawEn_lep1 > 30",
-        "isHggPho_lep1 == 1",
-        "isEBPho_lep1 == 1"
-    ])
-
-    sel_den_Si = "&&".join([
-        # "eleSCRawEn_lep1 < 40 && eleSCRawEn_lep1 > 30",
-        "elePresel_lep1 == 1",
-        "abs(eleSCEta_lep1) < 1.4442",
-        "category == 2"
-    ])
-
-    rdf_Zg = rdf_Zg.Filter(sel_den).Define("weight", "mcwei * puwei")
-    rdf_Si = rdf_Si.Filter(sel_den_Si).Define("weight", "mcwei * puwei")
-
-    main()
+    rconv = 16
+    pTbin = [15., 25., 40., 65, 150.]
+    reg = ["pT15to25", "pT25to40", "pT40to65", "pT65to150"]
+    for i in range(len(pTbin) - 1):
+        main(pTbin[i], pTbin[i+1], reg[i])
+        print(" ")
