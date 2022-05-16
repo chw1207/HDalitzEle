cc20894d229bac00cd4d7a3a7a2a696c062cf4ba
Cheng-Han <cheng-han.wu@cern.ch>
2022-05-16 16:57:49 +0800

    refinement and update of the training scripts

---
+++ Misc/ID_Trainer/Tools/hyperUtils.py
@@ -0,0 +1,32 @@
+import json
+import numpy as np
+from Tools.plotUtils import color
+
+# https://stackoverflow.com/a/70214170
+class trial_count:
+    """
+    A decorator that will count and print how many times the decorated function was called
+    """
+
+    def __init__(self, inline_func):
+        self.call_count = 0
+        self.inline_func = inline_func
+
+    def __call__(self, *args, **kwargs):
+        self.call_count += 1
+        self._print_call_count()
+        return self.inline_func(*args, **kwargs)
+
+    def _print_call_count(self):
+        print(color.CYAN + "[Trial {}] ".format(self.call_count) + color.END, flush=True)
+
+# https://stackoverflow.com/a/57915246
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
---
+++ Misc/ID_Trainer/config/TrainConfig_Merged2GsfID_EB_opt.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+#####################################################################
+# All plots, models, config file will be stored here
+OutputDirName = "./Results/Output_Merged2GsfID_hyperTune_Fall17_EB"
+Pickle_signal = "./data/Merged-ID/Dataframe_MergedID_HDalitz_Fall17_EB.pkl"
+Pickle_bkg = "./data/Merged-ID/Dataframe_MergedID_DYJets_QCD_Fall17_EB.pkl"
+Clfname = "MergedID"
+#####################################################################
+# prepare sample
+testsize = 0.2  #(0.2 means 20%)
+CommonCut = " and ".join([
+    "(eleCalibPt > 5.)",
+    "(elePresel == 1)",
+    "(nVtx > 1)",
+    "(nGsfMatchToReco >= 2)",
+    "(Class != 'Merged-1Gsf')"
+])
+#####################################################################
+Classes = ["Merged-2Gsf", "DYJets", "QCD"]
+ClassColors = ["#377eb8", "#3A9679", "#E16262"]
+MVA = "XGB"
+
+featureplotparam_json = "FeaturePlotParam_MergedID_EB.json"
+features = [
+    "rho",
+    "eleSCEta",
+    "eleSCRawEn",
+
+    "eledEtaAtVtx",
+    "eledPhiAtVtx",
+    "elePtError",
+    "eleHoverE",
+    "eleEoverP",
+    "eleEoverPout",
+    "eleEoverPInv",
+
+    "eleSCEtaWidth",
+    "eleSCPhiWidth",
+    "eleSigmaIEtaIEtaFull5x5",
+    "eleSigmaIPhiIPhiFull5x5",
+    "eleR9Full5x5",
+    "eleBrem",
+
+    "elePFChIso",
+    "elePFPhoIso",
+    "elePFNeuIso",
+
+    "gsfPtRatio",
+    "gsfDeltaR",
+    "gsfRelPtRatio"
+]
+
+opt = True # do the hyper parameter searching or not
+RandomState = 42
+model_params = {
+    "objective": "multi:softprob",
+    "num_class": len(Classes),
+    "eval_metric": "mlogloss",
+    "tree_method": "gpu_hist",
+    "random_state": RandomState
+}
+
+# if opt = False, this should be specified
+hyper_params = {
+    "learning_rate": 0.02216257774390921,
+    "max_delta_step": 10,
+    "max_depth": 12,
+    "min_child_weight": 134.62634063073708,
+    "min_split_loss": 0.23112380376907937,
+    "subsample": 0.7933362725563781
+}
+
+# number of trees
+num_boost_round = 2000
+early_stopping_rounds = 50
+#####################################################################
+# Reweighting scheme
+# ! Notice that "intwei" should be included in the input pkl files
+# * instwei: (relative xs) ggF as 1, for it being the production with the largest xs and VBF as xs_VBF/xs_ggF
+# * if uniformwt = True, then instwei will be assigned as 1. (treat electrons from different processes equally)
+# * Reweighing = Balanced(balanced reweight) | one of the class in "Classes"(pt-eta reweight)
+uniformwt = True
+Reweighing = "Merged-2Gsf"
+ptbins = [5, 15, 25, 35, 50, 65, 85, 120, 160, 200, 20000]
+etabins = [-1.479, -1.2, -0.8, -0.4, 0.0, 0.4, 0.8, 1.2, 1.479]
+ptwtvar = "eleCalibPt"
+etawtvar = "eleSCEta"
